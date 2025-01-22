82e3df4ddebe35d6b3ca3f6493c8f6def464a0d1
alexdenker <adenker@uni-bremen.de>
2025-01-22 11:04:36 +0000

    create empty files for epll and patchnr

---
+++ priors/epll.py
@@ -0,0 +1,7 @@
+"""
+Here will be the implementation for EPLL 
+
+Zoran et al., From learning models of natural image patches to whole image restoration, IEEE ICCV 2011
+https://ieeexplore.ieee.org/abstract/document/6126278
+
+"""
---
+++ priors/patchnr.py
@@ -0,0 +1,7 @@
+"""
+Here will be the implementation for the PatchNR 
+
+Altekrueger et al., PatchNR: learning from very few images by patch normalizing flow regularization, Inverse Problems 2023
+https://iopscience.iop.org/article/10.1088/1361-6420/acce5e
+
+"""
