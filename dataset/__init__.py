f3cc2b2e96a6ddb6a636d8bdc19173b0a7e044fc
Johannes Hertrich <j.hertrich@ucl.ac.uk>
2025-03-18 22:38:08 +0000

    cleanup

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,36 +1,40 @@
 from .datasets import BSDS500Dataset, FastMRISlices, BSD68
-from torchvision.transforms.v2 import ToTensor, Compose, Grayscale, ToDtype
+from torchvision.transforms.v2 import ToImage, Compose, Grayscale, ToDtype
 import torch
 
 
 def get_dataset(key, test=False, transform=None, root=None):
     if key == "BSDS500_gray":
         if transform is None:
-            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale()])
+            transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True), Grayscale()])
         else:
-            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale(), transform])
+            transforms = Compose(
+                [ToImage(), ToDtype(torch.float32, scale=True), Grayscale(), transform]
+            )
         return BSDS500Dataset(
             root="BSDS500", download=True, test=test, transform=transforms
         )
     elif key == "BSD68":
         if transform is None:
-            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale()])
+            transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True), Grayscale()])
         else:
-            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale(), transform])
+            transforms = Compose(
+                [ToImage(), ToDtype(torch.float32, scale=True), Grayscale(), transform]
+            )
         return BSD68(transform=transforms)
     # elif key == "BSDS500_color":
     #     if transform is None:
-    #         transforms = Compose([ToTensor(), ToDtype(torch.float32)])
+    #         transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True)])
     #     else:
-    #         transforms = Compose([ToTensor(), ToDtype(torch.float32), transform])
+    #         transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True), transform])
     #     return BSDS500Dataset(
     #         root="BSDS500", download=True, test=test, transform=transforms
     #     )
     # elif key == "fastMRI":
     #     if transform is None:
-    #         transforms = Compose([ToTensor(), ToDtype(torch.float32)])
+    #         transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True)])
     #     else:
-    #         transforms = Compose([ToTensor(), ToDtype(torch.float32), ToDtype(), transform])
+    #         transforms = Compose([ToImage(), ToDtype(torch.float32, scale=True), ToDtype(), transform])
     #     return FastMRISlices(root, test=test, transform=transforms)
     else:
         raise NameError("Unknown dataset!")
