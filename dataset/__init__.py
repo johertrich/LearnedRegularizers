d092f943c9fbe536bae0e05b8036696f7cd2a237
Johannes Hertrich <j.hertrich@ucl.ac.uk>
2025-03-18 21:41:24 +0000

    line search in evaluation + convert datasets automatically to float32

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,35 +1,36 @@
 from .datasets import BSDS500Dataset, FastMRISlices, BSD68
-from torchvision.transforms import ToTensor, Compose, Grayscale
+from torchvision.transforms.v2 import ToTensor, Compose, Grayscale, ToDtype
+import torch
 
 
 def get_dataset(key, test=False, transform=None, root=None):
     if key == "BSDS500_gray":
         if transform is None:
-            transforms = Compose([ToTensor(), Grayscale()])
+            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale()])
         else:
-            transforms = Compose([ToTensor(), Grayscale(), transform])
+            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale(), transform])
         return BSDS500Dataset(
             root="BSDS500", download=True, test=test, transform=transforms
         )
     elif key == "BSD68":
         if transform is None:
-            transforms = Compose([ToTensor(), Grayscale()])
+            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale()])
         else:
-            transforms = Compose([ToTensor(), Grayscale(), transform])
+            transforms = Compose([ToTensor(), ToDtype(torch.float32), Grayscale(), transform])
         return BSD68(transform=transforms)
     # elif key == "BSDS500_color":
     #     if transform is None:
-    #         transforms = ToTensor()
+    #         transforms = Compose([ToTensor(), ToDtype(torch.float32)])
     #     else:
-    #         transforms = Compose([ToTensor(), transform])
+    #         transforms = Compose([ToTensor(), ToDtype(torch.float32), transform])
     #     return BSDS500Dataset(
     #         root="BSDS500", download=True, test=test, transform=transforms
     #     )
     # elif key == "fastMRI":
     #     if transform is None:
-    #         transforms = ToTensor()
+    #         transforms = Compose([ToTensor(), ToDtype(torch.float32)])
     #     else:
-    #         transforms = Compose([ToTensor(), transform])
+    #         transforms = Compose([ToTensor(), ToDtype(torch.float32), ToDtype(), transform])
     #     return FastMRISlices(root, test=test, transform=transforms)
     else:
         raise NameError("Unknown dataset!")
--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,6 +1,6 @@
 import numpy as np
 from torch.utils.data import DataLoader
-from .nag import reconstruct_NAG
+from .nag_ls import reconstruct_NAG_LS
 import torch
 from deepinv.loss.metric import PSNR
 
@@ -26,36 +26,7 @@ def evaluate(
     for p in regularizer.parameters():
         p.requires_grad_(False)
 
-    def reconstruct(y):
-        # run Nesterov Accelerated Gradient
-
-        x = physics.A_dagger(y)
-        z = x.clone()
-        t = 1
-        res = NAG_tol + 1
-        for step in range(NAG_max_iter):
-            x_old = torch.clone(x)
-            grad = (
-                data_fidelity.grad(x, y, physics) + lmbd * regularizer.grad(x).detach()
-            )
-            x = z - NAG_step_size * grad
-            x = x.clamp(0, 1)
-            t_old = t
-            t = 0.5 * (1 + np.sqrt(1 + 4 * t**2))
-            z = x + (t_old - 1) / t * (x - x_old)
-
-            if step > 0:
-                res_vec = torch.sqrt(
-                    torch.sum((x - x_old).view(x.shape[0], -1) ** 2)
-                    / torch.sum(x.view(x.shape[0], -1) ** 2)
-                )
-                res = torch.max(res_vec)
-            if res < NAG_tol:
-                break
-        return x
-
     ## Evaluate on the test set
-
     psnrs = []
     for i, x in enumerate(dataloader):
         if device == "mps":
@@ -64,7 +35,7 @@ def evaluate(
         else:
             x = x.to(device).to(torch.float)
         y = physics(x)
-        recon = reconstruct_NAG(
+        recon = reconstruct_NAG_LS(
             y,
             physics,
             data_fidelity,
@@ -73,7 +44,6 @@ def evaluate(
             NAG_step_size,
             NAG_max_iter,
             NAG_tol,
-            detach_grads=True,
             verbose=verbose,
         )
         psnrs.append(psnr(recon, x).squeeze().item())
