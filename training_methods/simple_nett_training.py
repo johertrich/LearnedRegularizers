ba5605d581135dafd4235d993ebd0fb768a7812d
schwabjohannes <schwaebou@gmail.com>
2025-04-14 10:51:17 +0200

    update

--- training_methods/simple_nett_training.py
+++ training_methods/simple_nett_training.py
@@ -9,14 +9,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-def simple_NETT_training(model, train_dataloader, val_dataloader, device, optimizer, lr, num_epochs):
+def simple_NETT_training(model, train_dataloader, val_dataloader, device, optimizer, lr, num_epochs, save_best = False, weight_dir = None):
     criterion = nn.MSELoss()
     if optimizer == 'Adam':
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2)
+        optimizer = optim.Adam(model.parameters(),weight_decay = 1e-3, lr=lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
     
     model.to(device)
+    min_val_loss = 1.
     
     for epoch in range(num_epochs):
         model.train()
@@ -26,7 +28,9 @@ def simple_NETT_training(model, train_dataloader, val_dataloader, device, optimi
             
             optimizer.zero_grad()
             outputs = model(images)
-            loss = criterion(outputs, images_gt)
+            out_diff = images-outputs
+            trgt_diff = images-images_gt
+            loss = criterion(out_diff, trgt_diff)
             loss.backward()
             optimizer.step()
             
@@ -36,6 +40,31 @@ def simple_NETT_training(model, train_dataloader, val_dataloader, device, optimi
         
         model.eval()
         val_loss = 0.0
+        plt.subplot(241)
+        plt.imshow(outputs[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(242)
+        plt.imshow(images[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(243)
+        plt.imshow(outputs[0,0].detach().cpu()-images_gt[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(244)
+        plt.imshow(images[0,0].detach().cpu()-images_gt[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(245)
+        plt.imshow(out_diff[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(246)
+        plt.imshow(trgt_diff[0,0].detach().cpu(),cmap ='gray')
+        plt.colorbar()
+        plt.subplot(247)
+        plt.imshow(out_diff[0,0].detach().cpu()-trgt_diff[0,0].detach().cpu(),cmap = 'gray')
+        plt.colorbar()
+        plt.subplot(248)
+        plt.imshow(images_gt[0,0].detach().cpu(),cmap ='gray')
+        plt.colorbar()
+        plt.show()
         
         with torch.no_grad():
             for images, images_gt in val_dataloader:
@@ -43,8 +72,19 @@ def simple_NETT_training(model, train_dataloader, val_dataloader, device, optimi
                 outputs = model(images)
                 loss = criterion(outputs, images_gt)
                 val_loss += loss.item()
+                # plt.subplot(121)
+                # plt.imshow(outputs[0,0].detach().cpu()-images[0,0].detach().cpu())
+                # plt.subplot(122)
+                # plt.imshow(outputs[0,0].detach().cpu())
+                # plt.show()
         
         val_loss /= len(val_dataloader)
+        if val_loss < min_val_loss:
+            if save_best == True:
+                torch.save(model.state_dict(), weight_dir)
+                print('new min val loss: saving weights')
+            min_val_loss = val_loss
+            
         scheduler.step(val_loss)
         
         print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.8f}, Val Loss: {val_loss:.8f}")
