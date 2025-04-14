1dd1b629efc1f6f09730328a9a02a81c2c53e3f5
schwabjohannes <schwaebou@gmail.com>
2025-04-14 11:02:57 +0200

    update

--- example_NETT_inference.py
+++ example_NETT_inference.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  3 08:51:30 2025
+Created on Sun Mar 23 17:17:40 2025
 
 @author: Johannes
 """
 
-from priors import NETT
-import torch
-from deepinv.physics import Denoising, GaussianNoise, Tomography
-from training_methods import simple_NETT_training
-from deepinv.optim import L2
+from deepinv.physics import Denoising, MRI, GaussianNoise, Tomography
+from deepinv.optim import L2, Tikhonov
+from deepinv.utils.plotting import plot
+from evaluation import evaluate
 from dataset import get_dataset
-from dataset.utils import NETT_transform
-from torchvision.transforms import RandomCrop, CenterCrop, Resize
-from torch.utils.data import Subset as subset
-from torchvision import transforms
-import matplotlib.pyplot as plt
+from operators import MRIonR
+from torchvision.transforms import CenterCrop
+from priors import ICNNPrior, NETT, DRUNet
+import torch
 
 if torch.backends.mps.is_available():
     # mps backend is used in Apple Silicon chips
@@ -24,77 +22,91 @@ elif torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
+print("device: ", device)
+torch.random.manual_seed(0)  # make results deterministic
 
-problem = "Denoising"
-algorithm = "Adam"  # or "MAID", "Adam", "AdamW", "ISGD_Momentum"
+############################################################
 
-n_iter = 10000
-regularization_param = 0.001
-lam = 0.0005
-sample_idx = 55
+# Problem selection
 
-# problem dependent parameters
-if problem == "Tomography":
-    noise_level = 0.1
-    physics = Tomography(angles = 30, img_width = 128, circle = False)
-    NT = NETT_transform(0.0,physics)
-    resize_trans = Resize((128,128))
-    tran = transforms.Compose([resize_trans,NT])
-    dataset = get_dataset("BSDS500_gray", test=True, transform=tran)
+problem = "Denoising"  # Select problem setups, which we consider.
+only_first = False  # just evaluate on the first image of the dataset for test purposes
 
+############################################################
+
+# Define regularizer
+
+regularizer = DRUNet(in_channels=1,out_channels = 1,device = device)
+regularizer.load_state_dict(torch.load('weights/simple_DRUNETT_denoising.pt'))
+
+
+# reconstruction hyperparameters, might be problem dependent
+if problem == "Denoising":
+    lmbd = 0.7
+    # regularization parameter
+elif problem == "CT":
+    lmbd = 500.0  # regularization parameter
+
+# Parameters for the Nesterov Algorithm, might also be problem dependent...
+
+NAG_step_size = 1e-4  # step size in NAG
+NAG_max_iter = 5000  # maximum number of iterations in NAG
+NAG_tol = 1e-6  # tolerance for the relative error (stopping criterion)
+
+
+#############################################################
+############# Problem setup and evaluation ##################
+############# This should not be changed   ##################
+#############################################################
+
+# Define forward operator
 if problem == "Denoising":
     noise_level = 0.1
     physics = Denoising(noise_model=GaussianNoise(sigma=noise_level))
     data_fidelity = L2(sigma=1.0)
-    NT = NETT_transform(0.0,physics)
-    tran = transforms.Compose([RandomCrop(256),NT])
-    dataset = get_dataset("BSDS500_gray", test=False, transform=tran)
-    lmbd = 1
+    dataset = get_dataset("BSD68", transform=CenterCrop(320))
+elif problem == "CT":
+    noise_level = 0.5
+    dataset = get_dataset("BSD68", transform=CenterCrop(256))
+    imsize = dataset[0].shape[-1]
+    physics = Tomography(
+        imsize // 3, imsize, device=device, noise_model=GaussianNoise(sigma=noise_level)
+    )
+    data_fidelity = L2(sigma=1.0)
+# elif problem == "MRI":
+#    dataset = get_dataset("BSDS500_gray", transform=CenterCrop(256), test=True)
+#    img_size = dataset[0].shape
+#    noise_level = 0.05
+#    # simple Cartesian mask generation from the deepinv tour...
+#    mask = torch.rand((1, img_size[-1]), device=device) > 0.75
+#    mask = torch.ones((img_size[-2], 1), device=device) * mask
+#    mask[:, int(img_size[-1] / 2) - 2 : int(img_size[-1] / 2) + 2] = 1
+#    # The MRI operator in deepinv operates on complex-valued images.
+#    # The MRIonR operator wraps it for real-valued images
+#    physics = MRIonR(
+#        mask=mask, device=device, noise_model=GaussianNoise(sigma=noise_level)
+#    )
+#    data_fidelity = L2(sigma=1.0)
+else:
+    raise NotImplementedError("Problem not found")
 
+# Call unified evaluation routine
 
-# choose random image and generate data
+mean_psnr, x_out, y_out, recon_out = evaluate(
+    physics=physics,
+    data_fidelity=data_fidelity,
+    dataset=dataset,
+    regularizer=regularizer,
+    lmbd=lmbd,
+    NAG_step_size=NAG_step_size,
+    NAG_max_iter=NAG_max_iter,
+    NAG_tol=NAG_tol,
+    only_first=only_first,
+    device=device,
+    verbose=True,
+)
 
-image = dataset[sample_idx][0].unsqueeze(1).to(device)
-data = physics(image)
+# plot ground truth, observation and reconstruction for the first image from the test dataset
+plot([x_out, y_out, recon_out])
 
-# define regularizer
-
-regularizer = NETT(in_channels=1,out_channels = 1).to(device)
-regularizer.load_state_dict(torch.load('weights/simple_NETT_denoising.pt'))
-
-# Initialize with zero
-
-x = torch.zeros_like(image,requires_grad = True)
-x_land = torch.zeros_like(image,requires_grad = True)
-error = []
-error_land = []
-reg_value = []
-
-# Alternating gradient descent for the NETT functional
-
-for i in range(n_iter):
-    x = x.cpu()-lam*physics.A_adjoint(physics.A(x).cpu()-data.cpu())
-    x_land = x_land.cpu()-lam*physics.A_adjoint(physics.A(x_land).cpu()-data.cpu())
-    if i%100 == 0 and i>0:
-        plt.subplot(221)
-        plt.imshow(x_land[0,0].detach().cpu())
-        plt.subplot(222)
-        plt.imshow(x[0,0].detach())
-        plt.title('iteration '+str(i))
-        plt.subplot(223)
-        plt.plot(error[28:], label = 'NETT')
-        plt.plot(error_land[28:], label = 'Landweber')
-        plt.legend()
-        plt.subplot(224)
-        plt.plot(reg_value, label = 'regularization value')
-        plt.legend()
-        plt.show()
-
-    out = regularizer.regularizer(x.to(device))
-    grad = torch.autograd.grad(outputs=out, inputs=x, retain_graph=True)[0]
-    x = x - regularization_param*grad
-    error.append(torch.mean((x.detach().cpu()-image.cpu())**2))
-    error_land.append(torch.mean((x_land.detach().cpu()-image.cpu())**2))
-    reg_value.append(out.detach().cpu())
-    
-
+reg_denoised = regularizer(y_out)
