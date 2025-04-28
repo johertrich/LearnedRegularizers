70eb370e16b916342bd0fe6935a8f1010e7fa7e0
Matthias J. Ehrhardt <matthias.j.ehrhardt@gmail.com>
2025-04-28 14:23:41 +0100

    Add a bit of documentation to nmAPG.py

--- evaluation/nmAPG.py
+++ evaluation/nmAPG.py
@@ -2,11 +2,14 @@ import torch
 import numpy as np
 from typing import Callable
 
-# Implements Algorithm 4 from the paper
+# Implements Algorithm 4 from the supplementary material of
 #
 # Huan Li, Zhouchen Lin
 # Accelerated Proximal Gradient Methods for Nonconvex Programming
 # NeurIPS 2015
+#
+# [1] https://papers.nips.cc/paper_files/paper/2015/hash/f7664060cc52bc6f3d620bcedc94a4b6-Abstract.html
+# [2] https://papers.nips.cc/paper_files/paper/2015/file/f7664060cc52bc6f3d620bcedc94a4b6-Supplemental.zip
 
 
 def nmAPG(
@@ -23,36 +26,51 @@ def nmAPG(
     verbose: bool = False,
     return_L: bool = False,
 ):
-    x = x0.clone()
-    x_old = x.clone()
-    z = x0.clone()
-    t = 1.0
-    t_old = 0.0
-    q = 1.0
-    c = f(x, y)
+    '''
+    Algorithm 4: nonmonotone APG with line search
+    
+    Solve for a given y: min_x f(x, y)
+
+    In the notation of the paper F(x) = f(x, y).
+    '''
+
+    # initialize variables
+    x = x0.clone()             # Noation of the paper: x1
+    x_old = x.clone()          # x0
+    z = x0.clone()             # z1
+    t = 1.0                    # t1
+    t_old = 0.0                # t0
+    q = 1.0                    # q1
+    c = f(x, y)                # c1
     L = torch.full((x.shape[0], 1, 1, 1), L_init, dtype=torch.float32, device=x.device)
     res = (tol + 1) * torch.ones(x.shape[0], device=x.device, dtype=x.dtype)
     idx = torch.arange(0, x.shape[0], device=x.device)
-    grad = torch.zeros_like(x)
+    grad = torch.zeros_like(x) # nabla F(x)
     x_bar = torch.zeros_like(x)
+
+    # Main loop
     for i in range(max_iter):
         x_bar[idx] = (
             x[idx]
             + t_old / t * (z[idx] - x[idx])
             + (t_old - 1) / t * (x[idx] - x_old[idx])
-        )
+        ) # Eq 148, x_bar = yk
         x_old = x.clone()
         energy, grad[idx] = f(x_bar[idx], y[idx]), nabla(x_bar[idx], y[idx])
+
+        # NOT CHECKED YET
         if i > 0:
-            dx = grad[idx] - grad_old[idx]
-            s = (dx * dx).sum((1, 2, 3), keepdim=True)
+            dx = grad[idx] - grad_old[idx] # r in the paper
+            s = (dx * dx).sum((1, 2, 3), keepdim=True) # r^Tr
             L[idx] = torch.clip(
-                s / (dx * (x_bar[idx] - x_bar_old[idx])).sum((1, 2, 3), keepdim=True),
+                s / (dx * (x_bar[idx] - x_bar_old[idx])).sum((1, 2, 3), keepdim=True), # alpha_y = <s,r>/<r,r> in paper, Eq 150
                 min=1.0,
                 max=None,
-            )
+            ) # Why is there a lower bound of 1?
+
+        # Repeat Eq 151 and 152
         for ii in range(150):
-            z[idx] = x_bar[idx] - grad[idx] / L[idx]
+            z[idx] = x_bar[idx] - grad[idx] / L[idx] # Eq 151, 1/L = alpha_y
             dx = z[idx] - x_bar[idx]
             bound = torch.max(
                 energy[:, None, None, None], c[idx, None, None, None]
@@ -62,14 +80,17 @@ def nmAPG(
                 break
             L[idx] = torch.where(
                 energy_new[:, None, None, None] <= bound, L[idx], L[idx] / rho
-            )
+            ) # the reduction with rho is a bit different in the paper as it reduces it also when bound is successful. Not sure this is meant though
+
+        # If for Eq 153-158
         idx2 = (
             (energy_new[:] >= (c[idx] - delta * (dx * dx).sum((1, 2, 3))))
             .nonzero()
             .view(-1)
         )
         if idx2.nelement() > 0:
-            gradx = nabla(x[idx[idx2]], y[idx[idx2]])
+            gradx = nabla(x[idx[idx2]], y[idx[idx2]]) # nabla f(xk)
+            
             if i > 0:
                 dx = gradx - grad_old[idx[idx2]]
                 s = (dx * dx).sum((1, 2, 3), keepdim=True)
@@ -103,23 +124,25 @@ def nmAPG(
             x[tmp] = v[idx3]
         else:
             x[idx] = z[idx]
+
+        
         if i > 0:
             res[idx] = torch.norm(x[idx] - x_old[idx], p=2, dim=(1, 2, 3)) / torch.norm(
                 x[idx], p=2, dim=(1, 2, 3)
             )
         condition = res > tol
-        idx = condition.nonzero().view(-1)
+        idx = condition.nonzero().view(-1) # Update which data to still iterate on
 
         if torch.max(res) < tol:
             if verbose:
                 print(f"Converged in iter {i}, tol {torch.max(res).item():.6f}")
             break
         t_old = t
-        t = (np.sqrt(4.0 * t_old**2 + 1.0) + 1.0) / 2.0
+        t = (np.sqrt(4.0 * t_old**2 + 1.0) + 1.0) / 2.0 # Eq 159
         q_old = q
-        q = eta * q + 1.0
-        c[idx] = (eta * q_old * c[idx] + f(x[idx], y[idx])) / q
-        x_bar_old = x_bar.clone()
+        q = eta * q + 1.0 # Eq 160
+        c[idx] = (eta * q_old * c[idx] + f(x[idx], y[idx])) / q # Eq 161
+        x_bar_old = x_bar.clone() # Do we want to clone inside a loop?
         grad_old = grad.clone()
     if verbose and (torch.max(res) >= tol):
         print(f"max iter reached, tol {torch.max(res).item():.6f}")
@@ -142,6 +165,8 @@ def reconstruct_nmAPG(
     verbose=False,
     return_L=False,
 ):
+    '''wrapper for nmAPG'''
+
     if x_init is not None:
         # User-defined initialization or warm start
         x = torch.clone(x_init).detach()
