51485447676dfb1a09a4c48eb9b0d5cc7f21894f
hsw43 <120096671+hsw43@users.noreply.github.com>
2025-05-09 12:00:52 +0100

    Move simple ICNN code

---
+++ scripts_denoising_final/training_simple_ICNN_bilevel.py
@@ -0,0 +1,84 @@
+#%%
+from priors import simple_ICNNPrior
+import torch
+from deepinv.physics import Denoising, GaussianNoise
+from training_methods import bilevel_training
+from deepinv.optim import L2
+from dataset import get_dataset
+from torchvision.transforms import RandomCrop, CenterCrop, Compose
+from torchvision.transforms import (
+    RandomCrop,
+    RandomVerticalFlip,
+    Compose,
+    RandomHorizontalFlip,
+    CenterCrop,
+    RandomApply,
+    RandomRotation,
+)
+from priors.lpn.LPN_ICNN_prior import LPN_ICNNPrior
+
+
+if torch.backends.mps.is_available():
+    # mps backend is used in Apple Silicon chips
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+problem = "Denoising"
+
+# problem dependent parameters
+noise_level = 0.1
+physics = Denoising(noise_model=GaussianNoise(sigma=noise_level))
+data_fidelity = L2(sigma=1.0)
+lmbd = 1.0
+
+transform = Compose(
+    [
+        RandomCrop(64),
+        RandomHorizontalFlip(p=0.5),
+        RandomVerticalFlip(p=0.5),
+        RandomApply([RandomRotation((90, 90))], p=0.5),
+    ]
+)
+train_dataset = get_dataset("BSDS500_gray", test=False, transform=transform)
+val_dataset = get_dataset("BSDS500_gray", test=False, transform=CenterCrop(321))
+# splitting in training and validation set
+test_ratio = 0.1
+test_len = int(len(train_dataset) * 0.1)
+train_len = len(train_dataset) - test_len
+train_set = torch.utils.data.Subset(train_dataset, range(train_len))
+val_set = torch.utils.data.Subset(val_dataset, range(train_len, len(train_dataset)))
+
+# create dataloaders
+train_dataloader = torch.utils.data.DataLoader(
+    train_set, batch_size=48, shuffle=True, drop_last=True, num_workers=8
+)
+val_dataloader = torch.utils.data.DataLoader(
+    val_set, batch_size=1, shuffle=False, drop_last=True, num_workers=8
+)
+
+# define regularizer
+regularizer = simple_ICNNPrior(in_channels=1,channels=32,device=device)
+
+regularizer, loss_train, loss_val, psnr_train, psnr_val = bilevel_training(
+    regularizer,
+    physics,
+    data_fidelity,
+    lmbd,
+    train_dataloader,
+    val_dataloader,
+    epochs=100,
+    NAG_step_size=1e-1,
+    NAG_max_iter=1000,
+    NAG_tol_train=1e-6,
+    NAG_tol_val=1e-6,
+    lr=0.005,
+    lr_decay=0.99,
+    device=device,
+    verbose=False,
+)
+
+torch.save(regularizer.state_dict(), f"weights/ICNN_bilevel.pt")
+# %%
