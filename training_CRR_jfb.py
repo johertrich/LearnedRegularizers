4b96c9165341623e842abef57beaae2e53c4ef9b
Johannes Hertrich <j.hertrich@ucl.ac.uk>
2025-05-21 18:29:38 +0100

    CRR jfb

---
+++ training_CRR_jfb.py
@@ -0,0 +1,84 @@
+from priors import WCRR
+import torch
+from operators import get_operator
+from training_methods import bilevel_training
+from dataset import get_dataset
+from torchvision.transforms import RandomCrop, CenterCrop, Compose
+from torchvision.transforms import (
+    RandomCrop,
+    RandomVerticalFlip,
+    Compose,
+    RandomHorizontalFlip,
+    CenterCrop,
+    RandomApply,
+    RandomRotation,
+)
+
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+problem = "Denoising"
+
+physics, data_fidelity = get_operator(problem, device)
+
+transform = Compose(
+    [
+        RandomCrop(64),
+        RandomHorizontalFlip(p=0.5),
+        RandomVerticalFlip(p=0.5),
+        RandomApply([RandomRotation((90, 90))], p=0.5),
+    ]
+)
+train_dataset = get_dataset("BSDS500_gray", test=False, transform=transform)
+val_dataset = get_dataset("BSDS500_gray", test=False, transform=CenterCrop(321))
+# splitting in training and validation set
+test_ratio = 0.1
+test_len = int(len(train_dataset) * 0.1)
+train_len = len(train_dataset) - test_len
+train_set = torch.utils.data.Subset(train_dataset, range(train_len))
+val_set = get_dataset("BSD68")
+# val_set = torch.utils.data.Subset(val_dataset, range(train_len, len(train_dataset)))
+
+
+# create dataloaders
+train_dataloader = torch.utils.data.DataLoader(
+    train_set, batch_size=32, shuffle=True, drop_last=True, num_workers=8
+)
+val_dataloader = torch.utils.data.DataLoader(
+    val_set, batch_size=1, shuffle=False, drop_last=True, num_workers=8
+)
+
+# define regularizer
+noise_level = 0.1
+regularizer = WCRR(
+    sigma=noise_level,
+    weak_convexity=0.0,
+).to(device)
+
+lmbd = 1.0
+regularizer, loss_train, loss_val, psnr_train, psnr_val = bilevel_training(
+    regularizer,
+    physics,
+    data_fidelity,
+    lmbd,
+    train_dataloader,
+    val_dataloader,
+    epochs=200,
+    mode="JFB",
+    NAG_step_size=1e-1,
+    NAG_max_iter=1000,
+    NAG_tol_train=1e-4,
+    NAG_tol_val=1e-4,
+    lr=0.005,
+    lr_decay=.995,
+    device=device,
+    verbose=False,
+)
+
+torch.save(regularizer.state_dict(), f"weights/CRR_jfb.pt")
+
+# %%
--- training_methods/bilevel_training.py
+++ training_methods/bilevel_training.py
@@ -15,19 +15,21 @@ def bilevel_training(
     train_dataloader,
     val_dataloader,
     epochs=100,
+    mode="IFT",
     NAG_step_size=1e-1,
     NAG_max_iter=1000,
     NAG_tol_train=1e-4,
     NAG_tol_val=1e-4,
     minres_max_iter=1000,
     minres_tol=1e-6,
+    jfb_step_size_factor=1.0,
     lr=0.005,
     lr_decay=0.99,
     device="cuda" if torch.cuda.is_available() else "cpu",
     verbose=False,
+    validation_epochs=20,
     upper_loss=lambda x, y: torch.sum(((x - y) ** 2).view(x.shape[0], -1), -1),
 ):
-
     def hessian_vector_product(x, v, data_fidelity, y, regularizer, lmbd, physics):
         x = x.requires_grad_(True)
         grad = data_fidelity.grad(x, y, physics) + lmbd * regularizer.grad(x)
@@ -36,10 +38,14 @@ def bilevel_training(
         return hvp.detach()
 
     def jac_vector_product(x, v, data_fidelity, y, regularizer, lmbd, physics):
-        grad_lower_level = lambda x: data_fidelity.grad(x, y, physics) + lmbd * regularizer.grad(x)
+        grad_lower_level = lambda x: data_fidelity.grad(
+            x, y, physics
+        ) + lmbd * regularizer.grad(x)
         for param in regularizer.parameters():
             dot = torch.dot(grad_lower_level(x).view(-1), v.view(-1))
-            param.grad = -torch.autograd.grad(dot, param, create_graph=False)[0].detach()
+            param.grad = -torch.autograd.grad(dot, param, create_graph=False)[
+                0
+            ].detach()
         return regularizer
 
     optimizer = torch.optim.Adam(regularizer.parameters(), lr=lr)
@@ -64,11 +70,19 @@ def bilevel_training(
             x = x.to(device).to(torch.float32)
             y = physics(x)
             x_noisy = physics.A_dagger(y)
-            
-            x_recon = reconstruct_nmAPG(
-                y, physics, data_fidelity, regularizer, lmbd,
-                NAG_step_size, NAG_max_iter, NAG_tol_train,
-                verbose=verbose, x_init=x_noisy,
+
+            x_recon, L = reconstruct_nmAPG(
+                y,
+                physics,
+                data_fidelity,
+                regularizer,
+                lmbd,
+                NAG_step_size,
+                NAG_max_iter,
+                NAG_tol_train,
+                verbose=verbose,
+                x_init=x_noisy,
+                return_L=True,
             )
 
             optimizer.zero_grad()
@@ -76,19 +90,44 @@ def bilevel_training(
             train_loss_epoch += loss_fn(x_recon).item()
             train_psnr_epoch += psnr(x_recon, x).mean().item()
 
-            x_recon = x_recon.requires_grad_(True)
-            grad_loss = torch.autograd.grad(loss_fn(x_recon), x_recon, create_graph=False)[0].detach()
+            x_recon = x_recon.detach()
+            if mode == "IFT":
+                x_recon = x_recon.requires_grad_(True)
+                grad_loss = torch.autograd.grad(
+                    loss_fn(x_recon), x_recon, create_graph=False
+                )[0].detach()
 
-            q = minres(
-                lambda input: hessian_vector_product(x_recon.detach(), input, data_fidelity, y, regularizer, lmbd, physics),
-                grad_loss, max_iter=minres_max_iter, tol=minres_tol,
-            )
+                q = minres(
+                    lambda input: hessian_vector_product(
+                        x_recon.detach(),
+                        input,
+                        data_fidelity,
+                        y,
+                        regularizer,
+                        lmbd,
+                        physics,
+                    ),
+                    grad_loss,
+                    max_iter=minres_max_iter,
+                    tol=minres_tol,
+                )
 
-            regularizer = jac_vector_product(x_recon, q, data_fidelity, y, regularizer, lmbd, physics)
+                regularizer = jac_vector_product(
+                    x_recon, q, data_fidelity, y, regularizer, lmbd, physics
+                )
 
-            for param, reg in zip(optimizer.param_groups[0]["params"], regularizer.parameters()):
-                if param.grad is not None:
-                    param.grad = reg.grad
+                for param, reg in zip(
+                    optimizer.param_groups[0]["params"], regularizer.parameters()
+                ):
+                    if param.grad is not None:
+                        param.grad = reg.grad
+            elif mode == "JFB":
+                grad = data_fidelity.grad(
+                    x_recon, y, physics
+                ) + lmbd * regularizer.grad(x_recon)
+                x_recon = x_recon - jfb_step_size_factor / L * grad
+                loss = upper_loss(x_recon, x).mean()
+                loss.backward()
 
             optimizer.step()
 
@@ -98,41 +137,54 @@ def bilevel_training(
         loss_train.append(mean_train_loss)
         psnr_train.append(mean_train_psnr)
 
-        print(f"[Epoch {epoch+1}] Train Loss: {mean_train_loss:.2E}, PSNR: {mean_train_psnr:.2f}")
+        print(
+            f"[Epoch {epoch+1}] Train Loss: {mean_train_loss:.2E}, PSNR: {mean_train_psnr:.2f}"
+        )
 
         # ---- Validation ----
-        regularizer.eval()
-        with torch.no_grad():
-            val_loss_epoch = 0
-            val_psnr_epoch = 0
-            for x_val in tqdm(val_dataloader, desc=f"Epoch {epoch+1}/{epochs} - Val"):
-                x_val = x_val.to(device).to(torch.float32)
-                y_val = physics(x_val)
-                x_val_noisy = physics.A_dagger(y_val)
+        if (epoch + 1) % validation_epochs == 0:
+            regularizer.eval()
+            with torch.no_grad():
+                val_loss_epoch = 0
+                val_psnr_epoch = 0
+                for x_val in tqdm(
+                    val_dataloader, desc=f"Epoch {epoch+1}/{epochs} - Val"
+                ):
+                    x_val = x_val.to(device).to(torch.float32)
+                    y_val = physics(x_val)
+                    x_val_noisy = physics.A_dagger(y_val)
 
-                x_recon_val = reconstruct_nmAPG(
-                    y_val, physics, data_fidelity, regularizer, lmbd,
-                    NAG_step_size, NAG_max_iter, NAG_tol_val,
-                    verbose=verbose, x_init=x_val_noisy,
+                    x_recon_val = reconstruct_nmAPG(
+                        y_val,
+                        physics,
+                        data_fidelity,
+                        regularizer,
+                        lmbd,
+                        NAG_step_size,
+                        NAG_max_iter,
+                        NAG_tol_val,
+                        verbose=verbose,
+                        x_init=x_val_noisy,
+                    )
+
+                    val_loss_epoch += upper_loss(x_val, x_recon_val).mean().item()
+                    val_psnr_epoch += psnr(x_recon_val, x_val).mean().item()
+
+                mean_val_loss = val_loss_epoch / len(val_dataloader)
+                mean_val_psnr = val_psnr_epoch / len(val_dataloader)
+                loss_val.append(mean_val_loss)
+                psnr_val.append(mean_val_psnr)
+
+                print(
+                    f"[Epoch {epoch+1}] Val Loss: {mean_val_loss:.2E}, PSNR: {mean_val_psnr:.2f}"
                 )
 
-                val_loss_epoch += upper_loss(x_val, x_recon_val).mean().item()
-                val_psnr_epoch += psnr(x_recon_val, x_val).mean().item()
-
-            mean_val_loss = val_loss_epoch / len(val_dataloader)
-            mean_val_psnr = val_psnr_epoch / len(val_dataloader)
-            loss_val.append(mean_val_loss)
-            psnr_val.append(mean_val_psnr)
-
-            print(f"[Epoch {epoch+1}] Val Loss: {mean_val_loss:.2E}, PSNR: {mean_val_psnr:.2f}")
-
-            # ---- Save best regularizer based on validation PSNR ----
-            if mean_val_psnr > best_val_psnr:
-                best_val_psnr = mean_val_psnr
-                best_regularizer_state = copy.deepcopy(regularizer.state_dict())
+                # ---- Save best regularizer based on validation PSNR ----
+                if mean_val_psnr > best_val_psnr:
+                    best_val_psnr = mean_val_psnr
+                    best_regularizer_state = copy.deepcopy(regularizer.state_dict())
 
     # Load best regularizer
     regularizer.load_state_dict(best_regularizer_state)
 
     return regularizer, loss_train, loss_val, psnr_train, psnr_val
-
