058b4fd23ca4bf85518acde84f29164999839eed
alexdenker <adenker@uni-bremen.de>
2025-06-05 12:54:51 +0100

    use SilU activation and option to remove final bias

--- priors/local_ar.py
+++ priors/local_ar.py
@@ -11,25 +11,26 @@ import torch.nn as nn
 
 
 class cnn(nn.Module):
-    def __init__(self, in_channels=1):
+    def __init__(self, in_channels=1, use_bias=True):
         super(cnn, self).__init__()
 
-        self.act = nn.ReLU()
+        self.act = nn.SiLU()
         self.convnet = nn.Sequential(
-            nn.Conv2d(in_channels, 16, 3, padding=0),
+            nn.Conv2d(in_channels, 128, 3, padding=0, stride=1),
             self.act,
-            nn.Conv2d(16, 32, 3, padding=0),
+            nn.Conv2d(128, 64, 3, padding=0, stride=1),
             self.act,
-            nn.Conv2d(32, 32, 3, padding=0),
+            nn.Conv2d(64, 64, 3, padding=0, stride=1),
             self.act,
-            nn.Conv2d(32, 64, 3, padding=0),
+            nn.Conv2d(64, 64, 3, padding=0, stride=1),
             self.act,
-            nn.Conv2d(64, 64, 3, padding=0),
+            nn.Conv2d(64, 32, 3, padding=0, stride=1),
             self.act,
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=0),
             self.act,
-            nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(32, 1, kernel_size=3, stride=1, padding=0, bias=use_bias),
         )
+
 
     def forward(self, image):
         output = self.convnet(image)  # .squeeze().unsqueeze(-1)
@@ -49,12 +50,13 @@ class LocalAR(Prior):
         pad=True,
         device="cpu",
         pretrained=None,
+        use_bias=True
     ):
         super(LocalAR, self).__init__()
 
         self.device = device
 
-        self.cnn = cnn(in_channels=in_channels)
+        self.cnn = cnn(in_channels=in_channels, use_bias=use_bias)
         self.cnn.to(self.device)
 
         if pretrained is not None:
@@ -92,10 +94,11 @@ class LocalAR(Prior):
             )
 
         if self.n_patches == -1:
-            out = self.cnn(x).mean([1, 2, 3]).unsqueeze(-1)
+            #print("x.shape ", x.shape)
+            out = self.cnn(x).mean([1, 2, 3])
             # .mean([1,2,3]).unsqueeze(-1)
             patches, linear_inds = patch_extractor(x, self.n_patches, self.patch_size)
-
+            #print(patches.shape, linear_inds.shape, out.shape)
         else:
             patches, linear_inds = patch_extractor(x, self.n_patches, self.patch_size)
             B, n_patches = patches.shape[0:2]
@@ -109,15 +112,15 @@ class LocalAR(Prior):
             out = out.mean(1)
 
         if return_patch_per_pixel:
-            patch_per_pixel = torch.zeros_like(x).reshape(-1)
+            patch_per_pixel = torch.zeros(*x.shape[1:], device=x.device).reshape(-1)
             patch_per_pixel.index_put_(
-                (linear_inds,), torch.ones_like(patches).view(-1), accumulate=True
+                (linear_inds,), torch.ones_like(patches[0]).view(-1), accumulate=True
             )
-            patch_per_pixel = patch_per_pixel.reshape(x.shape)
+            patch_per_pixel = patch_per_pixel.reshape(x[0].shape)
 
-            return out, patch_per_pixel
+            return out, patch_per_pixel.unsqueeze(0)
         else:
-            return out
+            return out#.squeeze()
 
     def grad(self, x, *args, **kwargs):
         r"""
@@ -131,7 +134,7 @@ class LocalAR(Prior):
             nll, patch_per_pixel = self.g(x, return_patch_per_pixel=True)
             # print(nll)
             nll = nll.sum()
-            grad = torch.autograd.grad(outputs=nll, inputs=x)[0]
+            grad = torch.autograd.grad(outputs=nll, inputs=x, create_graph=True)[0]
             # print(torch.sum(grad**2))
             grad_norm = (patch_per_pixel + 1) / torch.max(patch_per_pixel)
             if self.pad:
@@ -142,18 +145,6 @@ class LocalAR(Prior):
                 grad_norm = grad_norm[
                     :, :, :, self.patch_size - 1 : -(self.patch_size - 1)
                 ]
-
-            # print(grad_norm.shape, grad.shape)
-
-            # import matplotlib.pyplot as plt
-            # fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(1,5)
-            # ax1.imshow(x[0,0].detach().cpu().numpy())
-            # ax2.imshow(grad[0,0].detach().cpu().numpy())
-            # ax3.imshow(patch_per_pixel[0,0].detach().cpu().numpy())
-            # ax4.imshow((grad /grad_norm)[0,0].detach().cpu().numpy())
-            # ax5.imshow(grad_norm[0,0].detach().cpu().numpy())
-
-            # plt.show()
 
         return grad / grad_norm
 
