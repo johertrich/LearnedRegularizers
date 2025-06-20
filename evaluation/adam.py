9b8a42dc2bece31c1b79948aed5482434dfca124
zeljkozeljko <zkereta@gmail.com>
2025-06-20 17:41:59 +0100

    adding tolerance for adam, modifying some arguments

--- evaluation/adam.py
+++ evaluation/adam.py
@@ -28,15 +28,6 @@ def reconstruct_adam(
     else:
         x = physics.A_dagger(y) 
 
-    # Remove 2-pixel boundary
-    x = x[:, :, 2:-2, 2:-2]
-    # Add replicate padding (2-pixel boundary restored with replicate padding)
-    x = F.pad(x, (2, 2, 2, 2), mode='replicate')
-    #import matplotlib.pyplot as plt 
-    #plt.figure()
-    #plt.imshow(x[0,0].detach().cpu().numpy(), cmap="gray")
-    #plt.show()
-
     def energy(val, y_in):
         fun = data_fidelity(val, y_in, physics) + lamda * regularizer.g(val)
 
@@ -47,6 +38,7 @@ def reconstruct_adam(
     optimizer = torch.optim.Adam([x], lr=step_size)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_iter, eta_min=step_size/10.)
     for i in (progress_bar := tqdm(range(max_iter))):
+        x_old = x.detach().clone()
         optimizer.zero_grad()
         loss = energy(x, y)     
         loss.backward()
@@ -64,14 +56,18 @@ def reconstruct_adam(
                     i + 1, loss.item()
                 )
             )
+
         scheduler.step() 
         with torch.no_grad():
             x.data.clamp_(min=0)
+        residual = torch.norm(x - x_old) / torch.norm(x_old)
+        if residual < tol:
+            if verbose:
+                print(
+                    "Converged after {} steps with residual {}".format(i + 1, residual)
+                )
+            break
 
-        #import matplotlib.pyplot as plt 
-        #plt.figure()
-        #plt.imshow(x[0,0].detach().cpu().numpy(), cmap="gray")
-        #plt.show()
 
     del optimizer 
     del scheduler
