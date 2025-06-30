e31f1eab3c461f8f9ee175821231667fa02f21b2
Sebastian Neumayer <41049175+Zeppo1994@users.noreply.github.com>
2025-06-30 16:25:56 +0200

    Update simple_ar_training.py

--- training_methods/simple_ar_training.py
+++ training_methods/simple_ar_training.py
@@ -6,6 +6,7 @@ Created on Wed Feb 26th 2025
 """
 
 import torch
+import copy
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
@@ -15,6 +16,7 @@ from dataset import get_dataset
 from torchvision.transforms import CenterCrop, RandomCrop
 from deepinv.utils import patch_extractor
 from torch.utils.data import DataLoader
+from deepinv.loss.metric import PSNR
 
 
 def WGAN_loss(regularizer, images, images_gt,mu=10):
@@ -28,20 +30,11 @@ def WGAN_loss(regularizer, images, images_gt,mu=10):
     net_interpolates = regularizer.g(interpolates)
     if (len(net_interpolates.shape) == 1):
         net_interpolates = net_interpolates.view(-1, 1)
-    fake = torch.Tensor(real_samples.shape[0], 1).fill_(1.0).type_as(real_samples).requires_grad_(False)
-    gradients = torch.autograd.grad(
-        outputs=net_interpolates,
-        inputs=interpolates,
-        grad_outputs=fake,
-        create_graph=True,
-        retain_graph=True,
-        only_inputs=True,
-    )[0]
-
+    gradients = regularizer.grad(net_interpolates)
     gradients = gradients.view(gradients.size(0), -1)
-    # print(model(real_samples).mean()-model(fake_samples).mean(),self.mu*(((gradients.norm(2, dim=1) - 1)) ** 2).mean())
-    loss = regularizer.g(real_samples).mean() - regularizer.g(fake_samples).mean()+mu*(((gradients.norm(2, dim=1) - 1)) ** 2).mean()
-    return loss
+    data_loss = regularizer.g(real_samples).mean() - regularizer.g(fake_samples).mean()
+    grad_loss = mu*(torch.clip(gradients.norm(2, dim=1) - 1, min=0.) ** 2).mean()
+    return data_loss + grad_loss,  grad_loss
 
 def estimate_lmbd(dataset,physics,device):
     lmbd=None
@@ -70,113 +63,102 @@ def simple_ar_training(
     lmbd,
     train_dataloader,
     val_dataloader,
-    epochs=25,
+    epochs=1000,
+    validation_epochs=100,
     lr=1e-3,
-    lr_decay=0.95,
+    lr_decay=0.998,
     device="cuda" if torch.cuda.is_available() else "cpu",
     mu = 10.0,
+    dynamic_range_psnr=False,
     save_str=None,
+    logger=None,
 ):
-    adversarial_loss = WGAN_loss
-    regularizer.to(device)
-    regularizer.train()
-    # optimizer = torch.optim.Adam(regularizer.parameters(), lr=lr)
-    optimizer = torch.optim.RMSprop(regularizer.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=lr_decay, patience=2)
+    assert validation_epochs <= epochs, (
+        "validation_epochs cannot be greater than epochs. "
+        "If validation_epochs > epochs, no validation will occur, "
+        "best_regularizer_state will remain unchanged, and the returned model will be identical to the initial state."
+    )
 
-    val_data = val_dataloader.dataset
-    if physics.__class__.__name__ == "denoising":
-        new_val_data = get_dataset("BSD68")
+    if dynamic_range_psnr:
+        psnr = PSNR(max_pixel=None)
+    else:
+        psnr = PSNR()
     
-    NAG_step_size = 1e-1  # step size in NAG
-    NAG_max_iter = 500  # maximum number of iterations in NAG
-    NAG_tol = 1e-4  # tolerance for therelative error (stopping criterion)
-    only_first = False
-    def eval_routine(val,lmbd_eval=lmbd):
-        mean_psnr, x_out, y_out, recon_out = evaluate(
-                    physics=physics,
-                    data_fidelity=data_fidelity,
-                    dataset=val,  # Access only the dataset of the dataloader
-                    regularizer=regularizer,
-                    lmbd=lmbd_eval,
-                    NAG_step_size=NAG_step_size,
-                    NAG_max_iter=NAG_max_iter,
-                    NAG_tol=NAG_tol,
-                    only_first=only_first,
-                    adaptive_range=True if physics.__class__.__name__ == "Tomography" else False,
-                    device=device,
-                    verbose=False,
-        )
-        for p in regularizer.parameters():
-            p.requires_grad_(True)
-        return mean_psnr, x_out, y_out, recon_out
-    if physics.__class__.__name__ == "Tomography":
-        mean_psnr, x_out, y_out, recon_out = eval_routine(val_data)
-        print("Initial reconstruction: ", mean_psnr)
-    else: 
-        mean_psnr, x_out, y_out, recon_out = eval_routine(val_data)
-        print("Initial reconstruction on BSD500: ", mean_psnr)
-        mean_psnr, x_out, y_out, recon_out = eval_routine(new_val_data)
-        lmbd_bsd68 = estimate_lmbd(new_val_data,physics,device)
-        print("Initial reconstruction on BSD68: ", mean_psnr)
-
-    best_psnr = mean_psnr
+    adversarial_loss = WGAN_loss
+    optimizer = torch.optim.Adam(regularizer.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=lr_decay)
+    
     for epoch in range(epochs):
         loss_vals = []
-        regularizer.train()
+        grad_loss_vals = []
         for x in tqdm(train_dataloader):
             optimizer.zero_grad()
-            if isinstance(x, list):
-                x = x[0]
-            if device == "mps":
-                x = x.to(torch.float32).to(device)
-            else:
-                x = x.to(device).to(torch.float)
+            x = x.to(device)
             y = physics(x)
             x_noisy = physics.A_dagger(y)
-            loss = adversarial_loss(regularizer, x_noisy, x, mu)
-        
-            mean_loss = torch.mean(loss)
-            mean_loss.backward()
+            
+            loss, grad_loss = adversarial_loss(regularizer, x_noisy, x, mu)
+            loss.backward()
             optimizer.step()
-            loss_vals.append(mean_loss.item())
-        print(
-            "Average training loss in epoch {0}: {1:.2E}".format(
-                epoch + 1, np.mean(loss_vals)
-            )
-        )
+            loss_vals.append(loss.item())
+            grad_loss_vals.append(grad_loss.item())
 
-        # with torch.no_grad():
-        
-        
-        loss_vals = []
-        # for x in tqdm(val_dataloader):
-        
-        mean_psnr, x_out, y_out, recon_out = eval_routine(val_data)
-        # print(mean_psnr)
-        loss_vals.append(mean_psnr.item())
-            
-        scheduler.step(np.mean(loss_vals))
-            
-        # print(
-        #     "Average validation loss in epoch {0}: {1:.2E}".format(
-        #         epoch + 1, np.mean(loss_vals)
-        #     )
-        # )
+        scheduler.step()
 
-        print("Learning rate: ", scheduler.get_last_lr()[0])
-        if mean_psnr > best_psnr:
-            best_psnr = mean_psnr
-            print("New best PSNR: ", best_psnr)
-            if save_str is not None: 
-                torch.save(regularizer.state_dict(), save_str)
-        if physics.__class__.__name__.lower() == "denoising":
-            print("---------------------")
-            mean_psnr, x_out, y_out, recon_out = eval_routine(new_val_data)
-            print("Initial reconstruction on BSD68: ", mean_psnr)
-            print("---------------------")
+        print_str = f"Average training loss in epoch {epoch + 1}: {np.mean(loss_vals):.2E}, average grad loss: {np.mean(grad_loss_vals):.2E}"
+        print(print_str)
+        if logger is not None:
+            logger.info(print_str)
 
+        if (epoch + 1) % validation_epochs == 0:
+            regularizer.eval()
+            with torch.no_grad():
+                val_loss_epoch = 0
+                val_psnr_epoch = 0
+                for x_val in tqdm(
+                    val_dataloader, desc=f"Epoch {epoch+1}/{epochs} - Val"
+                ):
+                    x_val = x_val.to(device).to(torch.float32)
+                    y_val = physics(x_val)
+                    x_val_noisy = physics.A_dagger(y_val)
 
+                    x_recon_val = reconstruct_nmAPG(
+                        y_val,
+                        physics,
+                        data_fidelity,
+                        regularizer,
+                        lmbd,
+                        NAG_step_size,
+                        NAG_max_iter,
+                        NAG_tol_val,
+                        verbose=verbose,
+                        x_init=x_val_noisy,
+                    )
+
+                    val_psnr_epoch += psnr(x_recon_val, x_val).mean().item()
+
+                mean_val_psnr = val_psnr_epoch / len(val_dataloader)
+                psnr_val.append(mean_val_psnr)
+                print_str = f"[Epoch {epoch+1}] Val Loss: {mean_val_loss:.2E}, PSNR: {mean_val_psnr:.2f}"
+                print(print_str)
+
+                if savestr is not None:
+                    torch.save(
+                        regularizer.state_dict(),
+                        savestr + "_epoch_" + str(epoch) + ".pt",
+                    )
+
+                if logger is not None:
+                    logger.info(print_str)
+
+                # ---- Save best regularizer based on validation PSNR ----
+                if mean_val_psnr > best_val_psnr:
+                    best_val_psnr = mean_val_psnr
+                    best_regularizer_state = copy.deepcopy(regularizer.state_dict())
+    # Load best regularizer
+    regularizer.load_state_dict(best_regularizer_state)
+
+    return regularizer, loss_train, loss_val, psnr_train, psnr_val
 
 
 # Training function for the LocalAR
