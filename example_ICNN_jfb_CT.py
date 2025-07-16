6a01d63d88f17d7002944ae83fe2f31d475c6746
hsw43 <120096671+hsw43@users.noreply.github.com>
2025-07-16 10:43:51 +0100

    Add ICNN CT training and evaluation

---
+++ example_ICNN_jfb_CT.py
@@ -0,0 +1,72 @@
+#%%
+from operators import get_evaluation_setting
+from deepinv.utils.plotting import plot
+from evaluation import evaluate
+from dataset import get_dataset
+from torchvision.transforms import CenterCrop
+from priors import simple_ICNNPrior
+import torch
+
+if torch.backends.mps.is_available():
+    # mps backend is used in Apple Silicon chips
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+print("device: ", device)
+torch.random.manual_seed(0)  # make results deterministic
+
+############################################################
+
+# Problem selection
+
+problem = "CT"  # Select problem setups, which we consider.
+only_first = False  # just evaluate on the first image of the dataset for test purposes
+
+############################################################
+
+# Define regularizer
+
+pretrained = "weights/ICNN_jfb_CT.pt"
+regularizer = simple_ICNNPrior(in_channels=1,channels=32,device=device)
+ckp = torch.load(pretrained, map_location=device)
+regularizer.load_state_dict(ckp)
+# reconstruction hyperparameters, might be problem dependent
+lmbd = 1.0
+
+# Parameters for the Nesterov Algorithm, might also be problem dependent...
+
+NAG_step_size = 1e-1  # step size in NAG
+NAG_max_iter = 1000  # maximum number of iterations in NAG
+NAG_tol = 1e-4  # tolerance for the relative error (stopping criterion)
+
+
+#############################################################
+############# Problem setup and evaluation ##################
+############# This should not be changed   ##################
+#############################################################
+
+# Define forward operator
+dataset, physics, data_fidelity = get_evaluation_setting(problem, device)
+
+# Call unified evaluation routine
+
+mean_psnr, x_out, y_out, recon_out = evaluate(
+    physics=physics,
+    data_fidelity=data_fidelity,
+    dataset=dataset,
+    regularizer=regularizer,
+    lmbd=lmbd,
+    NAG_step_size=NAG_step_size,
+    NAG_max_iter=NAG_max_iter,
+    NAG_tol=NAG_tol,
+    only_first=only_first,
+    adaptive_range=True,
+    device=device,
+    verbose=True,
+)
+
+# plot ground truth, observation and reconstruction for the first image from the test dataset
+plot([x_out, y_out, recon_out])
+# %%
